fe838efc857659e846a077a8ad02ee3144fbaab7
Bagaskara02 <138660569+Bagaskara02@users.noreply.github.com>
2025-05-23 17:26:50 +0700

    Add files via upload

---
+++ dataUser.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Bagaskara
+ */
+public class dataUser {
+    private Integer id_user;
+    private String username;
+    private String email_user;
+    private String password_user;
+
+    public Integer getId_user() {
+        return id_user;
+    }
+
+    public void setId_user(Integer id_user) {
+        this.id_user = id_user;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail_user() {
+        return email_user;
+    }
+
+    public void setEmail_user(String email_user) {
+        this.email_user = email_user;
+    }
+
+    public String getPassword_user() {
+        return password_user;
+    }
+
+    public void setPassword_user(String password_user) {
+        this.password_user = password_user;
+    }
+    
+    
+}
